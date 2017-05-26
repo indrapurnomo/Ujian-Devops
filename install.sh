7424d2b590c17b6d3618628a888918f852309975
Indra Purnomo <indra.purnomo@mail.ugm.ac.id>
2017-05-26 14:59:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ sudo apt-get install -y nginx curl vim
 echo "Nginx Config"
  sudo rm -rf /var/www/html/index.nginx-debian.html
  sudo cp -r /vagrant/blankon-linux-static-web/index.html /var/www/html
- sudo cp -r /vagrant/blankon-linux-static-web/tour_2.png /var/
-www/html
+ sudo cp -r /vagrant/blankon-linux-static-web/tour_2.png /var/www/html
 
  sudo service nginx restart
